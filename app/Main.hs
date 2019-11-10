1743ddd32e068635fb5c64b5c68c723c32b17038
Daniel <danielius.ritvas@gmail.com>
2019-11-10 18:55:50 +0200

    Implement dictionary validation and reading

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,70 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Char(digitToInt)
+
 data Move = Empty | Msg { coord :: (String, String)
                         , result :: String
                         , prev :: Move } deriving Show
 
-singleMove = "d6:result4:MISS5:coordd1:11:E1:21:2ee"
+singleMove = "d5:coordd1:11:D1:21:4e6:result4:MISSe"
 twoMoves = "d6:result4:MISS5:coordd1:11:E1:21:2e4:prevd5:coordd1:11:H1:22:10e6:result4:MISSee"
 twoMovesPrevFirst = "d4:prevd5:coordd1:11:H1:22:10e6:result4:MISSe6:result4:MISS5:coordd1:11:E1:21:2ee"
 
 game0 = "d4:prevd4:prevd5:coordd1:11:H1:22:10e4:prevd5:coordd1:11:I1:21:6e6:result4:MISS4:prevd5:coordd1:11:B1:21:5e4:prevd4:prevd5:coordd1:11:A1:21:2e4:prevd6:result4:MISS5:coordd1:11:G1:21:7e4:prevd6:result4:MISS5:coordd1:11:C1:21:4e4:prevd4:prevd5:coordd1:11:J1:21:9e6:result4:MISS4:prevd4:prevd5:coordd1:11:B1:21:6ee6:result4:MISS5:coordd1:11:G1:21:2eee6:result3:HIT5:coordd1:11:H1:21:9eeee6:result3:HITe5:coordd1:11:J1:21:2e6:result3:HITe6:result3:HITee6:result4:MISSe5:coordd1:11:E1:21:5e6:result4:MISSe6:result4:MISS5:coordd1:11:E1:21:2ee"
 
 main = do
-    let removedOuter = tryToRemoveOuterBoundaries singleMove
-    print removedOuter
+    let message = singleMove
+    let messageWithRemovedOuterBoundaries = tryToRemoveOuterBoundaries message
+    let (coord, tail) = readUpcommingKey messageWithRemovedOuterBoundaries
+    print $ readDictionary tail
 
+readDictionary :: String -> [String]
+readDictionary messageWithDictionaryAtTheBeggining = do
+    let (didRemoveFirst, d1) = removeFirstIfMatched 'd' messageWithDictionaryAtTheBeggining
+    if didRemoveFirst
+        then do
+            let (dictionaryContents, tail) = splitAtFirst 'e' d1
+            getDictionaryElements dictionaryContents []
+        else error $ createSyntaxError "Dictionary should start from 'd'"
+    where
+        getDictionaryElements :: String -> [String] -> [String]
+        getDictionaryElements "" arr = arr
+        getDictionaryElements dictionaryElementsAsBlob arr = do
+            let (_, value, tail) = getUpcommingDictionaryElement dictionaryElementsAsBlob
+            prependList value (getDictionaryElements tail arr)
+
+        getUpcommingDictionaryElement = readUpcommingValue
+
+
+
+-- Expects key:value at the begging of message
+readUpcommingValue :: String -> (String, String, String)
+readUpcommingValue message = do
+    let (keyLength, tailWithValue) = readUpcommingKey message
+    let (value, tail) = readUpcommingKey tailWithValue
+    (keyLength, value, tail)
+
+-- Possible key detection
+-- Error | Key and rest of the message
+readUpcommingKey :: String -> (String, String)
+readUpcommingKey message =
+    case message of
+    (keyLength:':':tail) -> do
+        let upcommingKey = take (digitToInt keyLength) tail
+        let restOfTheMessage = drop (digitToInt keyLength) tail
+        (upcommingKey, restOfTheMessage)
+    anyMessage ->
+        error "valueLength:value pair expected."
 
 -- Outer boundaries removal
-tryToRemoveOuterBoundaries :: String -> Either String String
+tryToRemoveOuterBoundaries :: String -> String
 tryToRemoveOuterBoundaries blob = do
     let (didRemoveFirst, b1) = removeFirstIfMatched 'd' blob
     let (didRemoveLast, b) = removeLastIfMatched 'e' b1
     if didRemoveFirst && didRemoveLast
-        then Right b
-        else Left "Outer boundary 'd' and 'e' expected."
+        then b
+        else error $ createSyntaxError "Outer boundaries 'd' and 'e' expected."
 
 removeFirstIfMatched :: Char -> String -> (Bool, String)
 removeFirstIfMatched _ [] = (False, [])
@@ -37,6 +78,20 @@ removeLastIfMatched c str = do
     let (didReverse, withLastRemoved) = removeFirstIfMatched c (reverse str)
     (didReverse, reverse withLastRemoved)
 
+createSyntaxError :: String -> String
+createSyntaxError errorMessage = "Syntax error: " ++ errorMessage
+
+splitAtFirst :: Eq a => a -> [a] -> ([a], [a])
+splitAtFirst x = fmap (drop 1) . break (x ==)
+
+get1st (a, _) = a
+get2nd (a, b) = b
+
+appendList :: Eq a => a -> [a] -> [a]
+appendList a = foldr (:) [a]
+
+prependList :: Eq a => a -> [a] -> [a]
+prependList element list = element:list
 
 -- parseCoordAfterPrev :: String -> Either String String
 -- parseCoordAfterPrev json = 
